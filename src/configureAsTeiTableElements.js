a047345f1c251d458edfae481d452bfd90be7bb1
Mathijs Meijer <mathijs.meijer@liones.nl>
2019-05-16 15:43:15 +0200

    DEV-2654 Convert to ES module format.

--- src/configureAsTeiTableElements.js
+++ src/configureAsTeiTableElements.js
@@ -1,45 +1,36 @@
-define([
-	'fontoxml-table-flow/configureAsTableElements',
+import configureAsTableElements from 'fontoxml-table-flow/src/configureAsTableElements.js';
+import TeiTableDefinition from './table-definition/TeiTableDefinition.js';
 
-	'./table-definition/TeiTableDefinition'
-], function (
-	configureAsTableElements,
-
-	TeiTableDefinition
-) {
-	'use strict';
-
-	/**
-	 * Configure TEI tables.
-	 *
-	 * @fontosdk
-	 *
-	 * @category add-on/fontoxml-table-flow-tei
-	 *
-	 * @param  {Object}  sxModule
-	 * @param  {Object}  [options]
-	 * @param  {number}  [options.priority]                    Selector priorty for all elements configured by this function
-	 * @param  {Object}  [options.table]                       Options for the table element
-	 * @param  {string}  [options.table.namespaceURI='']       The namespace URI for this table
-	 * @param  {Object}  [options.row]                         Configuration options for the row element
-	 * @param  {Object}  [options.row.headerAttribute]         Configuration options for an attribute to be set on a row element when it is a header row
-	 * @param  {string}  [options.row.headerAttribute.name]    The attribute name
-	 * @param  {string}  [options.row.headerAttribute.value]   The value to set
-	 * @param  {Object}  [options.row.regularAttribute]        Configuration options for an attribute to be set on a row element when it is not a header row
-	 * @param  {string}  [options.row.regularAttribute.name]   The attribute name
-	 * @param  {string}  [options.row.regularAttribute.value]  The value to set
-	 * @param  {Object}  [options.cell]                        Configuration options for the cell element
-	 * @param  {string}  [options.cell.defaultTextContainer]   The default text container for the cell element
-	 * @param  {Object}  [options.cell.headerAttribute]        Configuration options for an attribute to be set on a cell element when it is a header cell
-	 * @param  {string}  [options.cell.headerAttribute.name]   The attribute name
-	 * @param  {string}  [options.cell.headerAttribute.value]  The value to set
-	 * @param  {Object}  [options.cell.regularAttribute]       Configuration options for an attribute to be set on a cell element when it is not a header cell
-	 * @param  {string}  [options.cell.regularAttribute.name]  The attribute name
-	 * @param  {string}  [options.cell.regularAttribute.value] The value to set
-	 */
-	return function configureAsTeiTableElements (sxModule, options) {
-		options = options || {};
-		var tableDefinition = new TeiTableDefinition(options);
-		configureAsTableElements(sxModule, options, tableDefinition);
-	};
-});
+/**
+ * Configure TEI tables.
+ *
+ * @fontosdk
+ *
+ * @category add-on/fontoxml-table-flow-tei
+ *
+ * @param  {Object}  sxModule
+ * @param  {Object}  [options]
+ * @param  {number}  [options.priority]                    Selector priorty for all elements configured by this function
+ * @param  {Object}  [options.table]                       Options for the table element
+ * @param  {string}  [options.table.namespaceURI='']       The namespace URI for this table
+ * @param  {Object}  [options.row]                         Configuration options for the row element
+ * @param  {Object}  [options.row.headerAttribute]         Configuration options for an attribute to be set on a row element when it is a header row
+ * @param  {string}  [options.row.headerAttribute.name]    The attribute name
+ * @param  {string}  [options.row.headerAttribute.value]   The value to set
+ * @param  {Object}  [options.row.regularAttribute]        Configuration options for an attribute to be set on a row element when it is not a header row
+ * @param  {string}  [options.row.regularAttribute.name]   The attribute name
+ * @param  {string}  [options.row.regularAttribute.value]  The value to set
+ * @param  {Object}  [options.cell]                        Configuration options for the cell element
+ * @param  {string}  [options.cell.defaultTextContainer]   The default text container for the cell element
+ * @param  {Object}  [options.cell.headerAttribute]        Configuration options for an attribute to be set on a cell element when it is a header cell
+ * @param  {string}  [options.cell.headerAttribute.name]   The attribute name
+ * @param  {string}  [options.cell.headerAttribute.value]  The value to set
+ * @param  {Object}  [options.cell.regularAttribute]       Configuration options for an attribute to be set on a cell element when it is not a header cell
+ * @param  {string}  [options.cell.regularAttribute.name]  The attribute name
+ * @param  {string}  [options.cell.regularAttribute.value] The value to set
+ */
+export default function configureAsTeiTableElements(sxModule, options) {
+	options = options || {};
+	var tableDefinition = new TeiTableDefinition(options);
+	configureAsTableElements(sxModule, options, tableDefinition);
+}
--- src/table-definition/TeiTableDefinition.js
+++ src/table-definition/TeiTableDefinition.js
@@ -1,149 +1,189 @@
-define([
-	'fontoxml-table-flow/TableDefinition',
-	'fontoxml-table-flow/createCreateCellNodeStrategy',
-	'fontoxml-table-flow/createCreateRowStrategy',
-	'fontoxml-table-flow/setAttributeStrategies'
-], function (
-	TableDefinition,
-	createCreateCellNodeStrategy,
-	createCreateRowStrategy,
-	setAttributeStrategies
-) {
-	'use strict';
+import TableDefinition from 'fontoxml-table-flow/src/TableDefinition.js';
+import createCreateCellNodeStrategy from 'fontoxml-table-flow/src/createCreateCellNodeStrategy.js';
+import createCreateRowStrategy from 'fontoxml-table-flow/src/createCreateRowStrategy.js';
+import setAttributeStrategies from 'fontoxml-table-flow/src/setAttributeStrategies.js';
 
-	/**
-	 * Configures the table definition for TEI tables.
-	 *
-	 * @param {TeiTableOptions} options
-	 */
-	function TeiTableDefinition (options) {
-		var namespaceURI = options.table && options.table.namespaceURI ? options.table.namespaceURI : '';
+/**
+ * Configures the table definition for TEI tables.
+ *
+ * @param {TeiTableOptions} options
+ */
+function TeiTableDefinition(options) {
+	var namespaceURI =
+		options.table && options.table.namespaceURI ? options.table.namespaceURI : '';
 
-		var shouldSetAttributeForHeaderRows = options.row && options.row.headerAttribute;
-		var headerRowAttributeName = shouldSetAttributeForHeaderRows ?
-				options.row.headerAttribute.name : '';
-		var headerRowAttributeValue = shouldSetAttributeForHeaderRows ?
-				options.row.headerAttribute.value : '';
+	var shouldSetAttributeForHeaderRows = options.row && options.row.headerAttribute;
+	var headerRowAttributeName = shouldSetAttributeForHeaderRows
+		? options.row.headerAttribute.name
+		: '';
+	var headerRowAttributeValue = shouldSetAttributeForHeaderRows
+		? options.row.headerAttribute.value
+		: '';
 
-		var shouldSetAttributeForNormalRows = options.row && options.row.regularAttribute;
-		var normalRowAttributeName = shouldSetAttributeForNormalRows ?
-				options.row.regularAttribute.name : '';
-		var normalRowAttributeValue = shouldSetAttributeForNormalRows ?
-				options.row.regularAttribute.value : '';
+	var shouldSetAttributeForNormalRows = options.row && options.row.regularAttribute;
+	var normalRowAttributeName = shouldSetAttributeForNormalRows
+		? options.row.regularAttribute.name
+		: '';
+	var normalRowAttributeValue = shouldSetAttributeForNormalRows
+		? options.row.regularAttribute.value
+		: '';
 
-		var shouldSetAttributeForHeaderCells = options.cell && options.cell.headerAttribute;
-		var headerCellAttributeName = shouldSetAttributeForHeaderCells ?
-				options.cell.headerAttribute.name : '';
-		var headerCellAttributeValue = shouldSetAttributeForHeaderCells ?
-				options.cell.headerAttribute.value : '';
+	var shouldSetAttributeForHeaderCells = options.cell && options.cell.headerAttribute;
+	var headerCellAttributeName = shouldSetAttributeForHeaderCells
+		? options.cell.headerAttribute.name
+		: '';
+	var headerCellAttributeValue = shouldSetAttributeForHeaderCells
+		? options.cell.headerAttribute.value
+		: '';
 
-		var shouldSetAttributeForNormalCells = options.cell && options.cell.regularAttribute;
-		var normalCellAttributeName = shouldSetAttributeForNormalCells ?
-				options.cell.regularAttribute.name : '';
-		var normalCellAttributeValue = shouldSetAttributeForNormalCells ?
-				options.cell.regularAttribute.value : '';
+	var shouldSetAttributeForNormalCells = options.cell && options.cell.regularAttribute;
+	var normalCellAttributeName = shouldSetAttributeForNormalCells
+		? options.cell.regularAttribute.name
+		: '';
+	var normalCellAttributeValue = shouldSetAttributeForNormalCells
+		? options.cell.regularAttribute.value
+		: '';
 
-		var namespaceSelector = 'Q{' + namespaceURI + '}';
-		var selectorParts = {
-			table: namespaceSelector + 'table' + (options.table && options.table.tableFilterSelector ?
-					'[' + options.table.tableFilterSelector + ']' :
-					''),
-			row: namespaceSelector + 'row',
-			cell: namespaceSelector + 'cell'
-		};
+	var namespaceSelector = 'Q{' + namespaceURI + '}';
+	var selectorParts = {
+		table:
+			namespaceSelector +
+			'table' +
+			(options.table && options.table.tableFilterSelector
+				? '[' + options.table.tableFilterSelector + ']'
+				: ''),
+		row: namespaceSelector + 'row',
+		cell: namespaceSelector + 'cell'
+	};
 
-		var headerRowFilter = '';
-		var bodyRowFilter = '';
+	var headerRowFilter = '';
+	var bodyRowFilter = '';
 
-		if (shouldSetAttributeForHeaderRows) {
-			headerRowFilter += '[@' + headerRowAttributeName + '="' + headerRowAttributeValue + '"';
-			bodyRowFilter += '[(@' + headerRowAttributeName + '="' + headerRowAttributeValue + '") => not()';
-		}
-
-		if (shouldSetAttributeForHeaderCells) {
-			headerRowFilter += (headerRowFilter !== '' ? ' or ' : '[') + 'child::' + selectorParts.cell + '/@' + headerCellAttributeName + '="' + headerCellAttributeValue + '"';
-			bodyRowFilter += (bodyRowFilter !== '' ? ' or ' : '[') + '(child::' + selectorParts.cell + '/@' + headerCellAttributeName + '="' + headerCellAttributeValue + '") => not()';
-		}
-
-		if (shouldSetAttributeForHeaderRows || shouldSetAttributeForHeaderCells) {
-			headerRowFilter += ']';
-			bodyRowFilter += ']';
-		}
-
-		// Alias selector parts
-		var row = selectorParts.row;
-		var cell = selectorParts.cell;
-
-		// Properties object
-		var properties = {
-			selectorParts: selectorParts,
-
-			// Finds
-			findHeaderRowNodesXPathQuery: shouldSetAttributeForHeaderRows || shouldSetAttributeForHeaderCells ? './' + row + headerRowFilter : '()',
-			findBodyRowNodesXPathQuery: './' + row + bodyRowFilter,
-
-			findCellNodesXPathQuery: './' + cell,
-
-			findNonTableNodesPrecedingRowsXPathQuery: './*[self::' + row + ' => not() and following-sibling::' + row + ']',
-			findNonTableNodesFollowinggRowsXPathQuery: './*[self::' + row + ' => not() and preceding-sibling::' + row + ']',
-
-			// Data
-			getNumberOfColumnsXPathQuery: 'let $cols := ./@cols => number() return if ($cols) then $cols else ' +
-				'(let $cells := (.//' + row + ')[1]/' + cell + ' return for $node in $cells return let $cols := $node/@cols => number() return if ($cols) then $cols else 1) => sum()',
-			getRowSpanForCellNodeXPathQuery: 'let $rows := ./@rows => number() return if ($rows) then $rows else 1',
-			getColumnSpanForCellNodeXPathQuery: 'let $cols := ./@cols => number() return if ($cols) then $cols else 1',
-
-			// Creates
-			createCellNodeStrategy: createCreateCellNodeStrategy(namespaceURI, 'cell'),
-			createRowStrategy: createCreateRowStrategy(namespaceURI, 'row'),
-
-			// Set attributes
-			setTableNodeAttributeStrategies: [
-					setAttributeStrategies.createColumnCountAsAttributeStrategy('cols'),
-					setAttributeStrategies.createRowCountAsAttributeStrategy('rows')
-				],
-
-			setRowNodeAttributeStrategies: [],
-
-			setCellNodeAttributeStrategies: [
-					setAttributeStrategies.createColumnSpanAsAttributeStrategy('cols'),
-					setAttributeStrategies.createRowSpanAsAttributeStrategy('rows')
-				],
-		};
-
-		if (shouldSetAttributeForHeaderRows) {
-			properties.setRowNodeAttributeStrategies.push(
-				setAttributeStrategies.createHeaderDefinitionAsAttributeStrategy(
-					headerRowAttributeName,
-					headerRowAttributeValue));
-		}
-
-		if (shouldSetAttributeForNormalRows) {
-			properties.setRowNodeAttributeStrategies.push(
-				setAttributeStrategies.createNormalDefinitionAsAttributeStrategy(
-					normalRowAttributeName,
-					normalRowAttributeValue));
-		}
-
-		if (shouldSetAttributeForHeaderCells) {
-			properties.setCellNodeAttributeStrategies.push(
-				setAttributeStrategies.createHeaderDefinitionAsAttributeStrategy(
-					headerCellAttributeName,
-					headerCellAttributeValue));
-		}
-
-		if (shouldSetAttributeForNormalCells) {
-			properties.setCellNodeAttributeStrategies.push(
-				setAttributeStrategies.createNormalDefinitionAsAttributeStrategy(
-					normalCellAttributeName,
-					normalCellAttributeValue));
-		}
-
-		TableDefinition.call(this, properties);
+	if (shouldSetAttributeForHeaderRows) {
+		headerRowFilter += '[@' + headerRowAttributeName + '="' + headerRowAttributeValue + '"';
+		bodyRowFilter +=
+			'[(@' + headerRowAttributeName + '="' + headerRowAttributeValue + '") => not()';
 	}
 
-	TeiTableDefinition.prototype = Object.create(TableDefinition.prototype);
-	TeiTableDefinition.prototype.constructor = TeiTableDefinition;
+	if (shouldSetAttributeForHeaderCells) {
+		headerRowFilter +=
+			(headerRowFilter !== '' ? ' or ' : '[') +
+			'child::' +
+			selectorParts.cell +
+			'/@' +
+			headerCellAttributeName +
+			'="' +
+			headerCellAttributeValue +
+			'"';
+		bodyRowFilter +=
+			(bodyRowFilter !== '' ? ' or ' : '[') +
+			'(child::' +
+			selectorParts.cell +
+			'/@' +
+			headerCellAttributeName +
+			'="' +
+			headerCellAttributeValue +
+			'") => not()';
+	}
 
-	return TeiTableDefinition;
-});
+	if (shouldSetAttributeForHeaderRows || shouldSetAttributeForHeaderCells) {
+		headerRowFilter += ']';
+		bodyRowFilter += ']';
+	}
+
+	// Alias selector parts
+	var row = selectorParts.row;
+	var cell = selectorParts.cell;
+
+	// Properties object
+	var properties = {
+		selectorParts: selectorParts,
+
+		// Finds
+		findHeaderRowNodesXPathQuery:
+			shouldSetAttributeForHeaderRows || shouldSetAttributeForHeaderCells
+				? './' + row + headerRowFilter
+				: '()',
+		findBodyRowNodesXPathQuery: './' + row + bodyRowFilter,
+
+		findCellNodesXPathQuery: './' + cell,
+
+		findNonTableNodesPrecedingRowsXPathQuery:
+			'./*[self::' + row + ' => not() and following-sibling::' + row + ']',
+		findNonTableNodesFollowinggRowsXPathQuery:
+			'./*[self::' + row + ' => not() and preceding-sibling::' + row + ']',
+
+		// Data
+		getNumberOfColumnsXPathQuery:
+			'let $cols := ./@cols => number() return if ($cols) then $cols else ' +
+			'(let $cells := (.//' +
+			row +
+			')[1]/' +
+			cell +
+			' return for $node in $cells return let $cols := $node/@cols => number() return if ($cols) then $cols else 1) => sum()',
+		getRowSpanForCellNodeXPathQuery:
+			'let $rows := ./@rows => number() return if ($rows) then $rows else 1',
+		getColumnSpanForCellNodeXPathQuery:
+			'let $cols := ./@cols => number() return if ($cols) then $cols else 1',
+
+		// Creates
+		createCellNodeStrategy: createCreateCellNodeStrategy(namespaceURI, 'cell'),
+		createRowStrategy: createCreateRowStrategy(namespaceURI, 'row'),
+
+		// Set attributes
+		setTableNodeAttributeStrategies: [
+			setAttributeStrategies.createColumnCountAsAttributeStrategy('cols'),
+			setAttributeStrategies.createRowCountAsAttributeStrategy('rows')
+		],
+
+		setRowNodeAttributeStrategies: [],
+
+		setCellNodeAttributeStrategies: [
+			setAttributeStrategies.createColumnSpanAsAttributeStrategy('cols'),
+			setAttributeStrategies.createRowSpanAsAttributeStrategy('rows')
+		]
+	};
+
+	if (shouldSetAttributeForHeaderRows) {
+		properties.setRowNodeAttributeStrategies.push(
+			setAttributeStrategies.createHeaderDefinitionAsAttributeStrategy(
+				headerRowAttributeName,
+				headerRowAttributeValue
+			)
+		);
+	}
+
+	if (shouldSetAttributeForNormalRows) {
+		properties.setRowNodeAttributeStrategies.push(
+			setAttributeStrategies.createNormalDefinitionAsAttributeStrategy(
+				normalRowAttributeName,
+				normalRowAttributeValue
+			)
+		);
+	}
+
+	if (shouldSetAttributeForHeaderCells) {
+		properties.setCellNodeAttributeStrategies.push(
+			setAttributeStrategies.createHeaderDefinitionAsAttributeStrategy(
+				headerCellAttributeName,
+				headerCellAttributeValue
+			)
+		);
+	}
+
+	if (shouldSetAttributeForNormalCells) {
+		properties.setCellNodeAttributeStrategies.push(
+			setAttributeStrategies.createNormalDefinitionAsAttributeStrategy(
+				normalCellAttributeName,
+				normalCellAttributeValue
+			)
+		);
+	}
+
+	TableDefinition.call(this, properties);
+}
+
+TeiTableDefinition.prototype = Object.create(TableDefinition.prototype);
+TeiTableDefinition.prototype.constructor = TeiTableDefinition;
+
+export default TeiTableDefinition;
