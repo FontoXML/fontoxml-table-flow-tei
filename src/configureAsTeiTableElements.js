a2733d5bba90b3f023bd273e5b1517c8e79008ab
Mehmet Coskun <mehmet.coskun@fontoxml.com>
2019-08-26 12:25:36 +0200

    DEV-4016 Added api docs and example usage for table widgets

--- src/configureAsTeiTableElements.js
+++ src/configureAsTeiTableElements.js
@@ -4,30 +4,59 @@ import TeiTableDefinition from './table-definition/TeiTableDefinition.js';
 /**
  * Configure TEI tables.
  *
+ * Example usage for the table widgets:
+ *
+ *```
+ * 	configureAsTeiTableElements(sxModule, {
+ * 		table: {
+ *			localName: 'table'
+ *		},
+ *		entry: {
+ *			defaultTextContainer: 'p'
+ *		},
+ *		columnBefore: [createIconWidget('clock-o', {
+ *			clickOperation: 'lcTime-value-edit',
+ *			tooltipContent: 'Click here to edit the duration'
+ *		})],
+ *		rowBefore: [
+ *				createIconWidget('dot-circle-o', {
+ *					clickOperation: 'do-nothing'
+ *					clickOperation: 'do-nothing'
+ *				})
+ *			],
+ *		showInsertionWidget: true,
+ *		showHighlightingWidget: true
+ *	});
+ *```
+ *
  * @fontosdk
  *
  * @category add-on/fontoxml-table-flow-tei
  *
- * @param  {Object}  sxModule
- * @param  {Object}  [options]
- * @param  {number}  [options.priority]                    Selector priorty for all elements configured by this function
- * @param  {Object}  [options.table]                       Options for the table element
- * @param  {string}  [options.table.namespaceURI='']       The namespace URI for this table
- * @param  {Object}  [options.row]                         Configuration options for the row element
- * @param  {Object}  [options.row.headerAttribute]         Configuration options for an attribute to be set on a row element when it is a header row
- * @param  {string}  [options.row.headerAttribute.name]    The attribute name
- * @param  {string}  [options.row.headerAttribute.value]   The value to set
- * @param  {Object}  [options.row.regularAttribute]        Configuration options for an attribute to be set on a row element when it is not a header row
- * @param  {string}  [options.row.regularAttribute.name]   The attribute name
- * @param  {string}  [options.row.regularAttribute.value]  The value to set
- * @param  {Object}  [options.cell]                        Configuration options for the cell element
- * @param  {string}  [options.cell.defaultTextContainer]   The default text container for the cell element
- * @param  {Object}  [options.cell.headerAttribute]        Configuration options for an attribute to be set on a cell element when it is a header cell
- * @param  {string}  [options.cell.headerAttribute.name]   The attribute name
- * @param  {string}  [options.cell.headerAttribute.value]  The value to set
- * @param  {Object}  [options.cell.regularAttribute]       Configuration options for an attribute to be set on a cell element when it is not a header cell
- * @param  {string}  [options.cell.regularAttribute.name]  The attribute name
- * @param  {string}  [options.cell.regularAttribute.value] The value to set
+ * @param  {Object}          sxModule
+ * @param  {Object}          [options]
+ * @param  {number}          [options.priority]                    Selector priorty for all elements configured by this function
+ * @param  {boolean}         [options.showInsertionWidget]         To add insertion buttons which insert a column or a row to a specific place, default false.
+ * @param  {boolean}         [options.showHighlightingWidget]      To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
+ * @param  {Widget[]|null}   [options.columnBefore]                To add column icon widgets by using {@link createIconWidget}. Any widget can be added but only icon widget is supported.
+ * @param  {Widget[]|null}   [options.rowBefore]                   To add row icon widgets by using {@link createIconWidget}. Any widget can be added but only icon widget is supported.
+ * @param  {Object}          [options.table]                       Options for the table element
+ * @param  {string}          [options.table.namespaceURI='']       The namespace URI for this table
+ * @param  {Object}          [options.row]                         Configuration options for the row element
+ * @param  {Object}          [options.row.headerAttribute]         Configuration options for an attribute to be set on a row element when it is a header row
+ * @param  {string}          [options.row.headerAttribute.name]    The attribute name
+ * @param  {string}          [options.row.headerAttribute.value]   The value to set
+ * @param  {Object}          [options.row.regularAttribute]        Configuration options for an attribute to be set on a row element when it is not a header row
+ * @param  {string}          [options.row.regularAttribute.name]   The attribute name
+ * @param  {string}          [options.row.regularAttribute.value]  The value to set
+ * @param  {Object}          [options.cell]                        Configuration options for the cell element
+ * @param  {string}          [options.cell.defaultTextContainer]   The default text container for the cell element
+ * @param  {Object}          [options.cell.headerAttribute]        Configuration options for an attribute to be set on a cell element when it is a header cell
+ * @param  {string}          [options.cell.headerAttribute.name]   The attribute name
+ * @param  {string}          [options.cell.headerAttribute.value]  The value to set
+ * @param  {Object}          [options.cell.regularAttribute]       Configuration options for an attribute to be set on a cell element when it is not a header cell
+ * @param  {string}          [options.cell.regularAttribute.name]  The attribute name
+ * @param  {string}          [options.cell.regularAttribute.value] The value to set
  */
 export default function configureAsTeiTableElements(sxModule, options) {
 	options = options || {};
