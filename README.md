3da8e78f699d5f8beb964282ddb20376827b9627
Waqas Abbasi <waqas.abbasi@liones.nl>
2020-11-24 15:15:14 +0000

    DEV-8242 Add collapsible table support to Tei tables

--- README.md
+++ README.md
@@ -58,6 +58,17 @@ configureAsTeiTableElements(sxModule, {
 	// To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
 	showHighlightingWidget: true,
 
+	// This XPath expression determines whether or not a table has the ability to be collapsed. Optional, defaults to 'false()'.
+	// $rowCount and $columnCount helper variables can also optionally be used in the XPath expression to make it easier to configure
+	// when the table should collapse i.e. '$rowCount > 5' which will allow tables with rows more than 5 to be able to be collapsed/uncollapsed 
+	isCollapsibleQuery: 'false()'
+    
+	// This XPath expression determines whether a table that has the ability to be collapsed should start off as collapsed on initial load. Optional, defaults to 'true()'.
+	// $rowCount and $columnCount helper variables can also optionally be used in the XPath expression to make it easier to configure
+	// when the table should start off as collapsed i.e. '$rowCount > 10' means that tables that have more than 10 rows will initially start off as collapsed
+	// Note: This query is only evaluated on tables which have the ability to be collapsed using isCollapsibleQuery 
+	isInitiallyCollapsedQuery: 'true()'
+
 	// In TEI table, there are some operations in the column/row widget menus as default. But they can be overridden.
  	columnWidgetMenuOperations: [{ name: 'column-delete-at-index' }],
  	rowWidgetMenuOperations:[
--- src/configureAsTeiTableElements.js
+++ src/configureAsTeiTableElements.js
@@ -52,36 +52,40 @@ import TeiTableDefinition from './table-definition/TeiTableDefinition.js';
  *	});
  * ```
  *
+ * Tei tables can also be configured to be collapsible. Refer to {@link fonto-documentation/docs/editor/fontoxml-editor-documentation/quickstarts/configure-tables.xml#id-6c3f43af-b40c-4fa3-ab47-f0fd2d4ab85c our guide} to learn more.
+ *
  * @fontosdk
  *
  * @category add-on/fontoxml-table-flow-tei
  *
  * @param  {Object}          sxModule
  * @param  {Object}          [options]
- * @param  {number}          [options.priority]                    Selector priority for all elements configured by this function
- * @param  {boolean}         [options.showInsertionWidget]         To add insertion buttons which insert a column or a row to a specific place, default false.
- * @param  {boolean}         [options.showHighlightingWidget]      To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
- * @param  {Widget[]|null}   [options.rowBefore]                   To add row icon widgets by using {@link createIconWidget}. Row widgets are linked to the row elements of the table. Any widget can be added but only icon widget is supported.
- * @param  {Object[]|null}   [options.columnWidgetMenuOperations]  To configure table widget menu for columns. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
- * @param  {Object[]|null}   [options.rowWidgetMenuOperations]     To configure table widget menu for rows. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
- * @param  {Object}          [options.table]                       Options for the table element
- * @param  {string}          [options.table.namespaceURI='']       The namespace URI for this table
- * @param  {Object}          [options.row]                         Configuration options for the row element
- * @param  {Object}          [options.row.headerAttribute]         Configuration options for an attribute to be set on a row element when it is a header row
- * @param  {string}          [options.row.headerAttribute.name]    The attribute name
- * @param  {string}          [options.row.headerAttribute.value]   The value to set
- * @param  {Object}          [options.row.regularAttribute]        Configuration options for an attribute to be set on a row element when it is not a header row
- * @param  {string}          [options.row.regularAttribute.name]   The attribute name
- * @param  {string}          [options.row.regularAttribute.value]  The value to set
- * @param  {Object}          [options.cell]                        Configuration options for the cell element
- * @param  {string}          [options.cell.defaultTextContainer]   The default text container for the cell element
- * @param  {Object}          [options.cell.headerAttribute]        Configuration options for an attribute to be set on a cell element when it is a header cell
- * @param  {string}          [options.cell.headerAttribute.name]   The attribute name
- * @param  {string}          [options.cell.headerAttribute.value]  The value to set
- * @param  {Object}          [options.cell.regularAttribute]       Configuration options for an attribute to be set on a cell element when it is not a header cell
- * @param  {string}          [options.cell.regularAttribute.name]  The attribute name
- * @param  {string}          [options.cell.regularAttribute.value] The value to set
- * @param  {boolean}         [options.useDefaultContextMenu=true]  Whether or not to use a preconfigured context menu for elements within the table
+ * @param  {number}          [options.priority]                          Selector priority for all elements configured by this function
+ * @param  {boolean}         [options.showInsertionWidget]               To add insertion buttons which insert a column or a row to a specific place, default false.
+ * @param  {boolean}         [options.showHighlightingWidget]            To add highlighting bars which highlight columns and rows, and provide operations popover, default false.
+ * @param  {Widget[]|null}   [options.rowBefore]                         To add row icon widgets by using {@link createIconWidget}. Row widgets are linked to the row elements of the table. Any widget can be added but only icon widget is supported.
+ * @param  {Object[]|null}   [options.columnWidgetMenuOperations]        To configure table widget menu for columns. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
+ * @param  {Object[]|null}   [options.rowWidgetMenuOperations]           To configure table widget menu for rows. It accepts an array of {@link ContextualOperation}s, but only supports "name" and "contents" properties. It is allowed to have only one layer of menu.
+ * @param  {Object}          [options.table]                             Options for the table element
+ * @param  {string}          [options.table.namespaceURI='']             The namespace URI for this table
+ * @param  {Object}          [options.row]                               Configuration options for the row element
+ * @param  {Object}          [options.row.headerAttribute]               Configuration options for an attribute to be set on a row element when it is a header row
+ * @param  {string}          [options.row.headerAttribute.name]          The attribute name
+ * @param  {string}          [options.row.headerAttribute.value]         The value to set
+ * @param  {Object}          [options.row.regularAttribute]              Configuration options for an attribute to be set on a row element when it is not a header row
+ * @param  {string}          [options.row.regularAttribute.name]         The attribute name
+ * @param  {string}          [options.row.regularAttribute.value]        The value to set
+ * @param  {Object}          [options.cell]                              Configuration options for the cell element
+ * @param  {string}          [options.cell.defaultTextContainer]         The default text container for the cell element
+ * @param  {Object}          [options.cell.headerAttribute]              Configuration options for an attribute to be set on a cell element when it is a header cell
+ * @param  {string}          [options.cell.headerAttribute.name]         The attribute name
+ * @param  {string}          [options.cell.headerAttribute.value]        The value to set
+ * @param  {Object}          [options.cell.regularAttribute]             Configuration options for an attribute to be set on a cell element when it is not a header cell
+ * @param  {string}          [options.cell.regularAttribute.name]        The attribute name
+ * @param  {string}          [options.cell.regularAttribute.value]       The value to set
+ * @param  {boolean}         [options.useDefaultContextMenu=true]        Whether or not to use a preconfigured context menu for elements within the table
+ * @param  {XPathQuery}      [options.isCollapsibleQuery=false()]        The {@link XPathQuery} to determine whether or not a table has the ability to be collapsible. Optional, defaults to 'false()'. $rowCount and $columnCount helper variables can optionally be used in this XPath expression which evaluate to the total rows and total columns in a table.
+ * @param  {XPathQuery}      [options.isInitiallyCollapsedQuery=true()]  The {@link XPathQuery} to determine whether or not a table should initially start off as collapsed. Tables must first have the ability to be collapsible with isCollapsibleQuery. Optional, defaults to 'true()'. $rowCount and $columnCount helper variables can optionally be used in this XPath expression which evaluate to the total rows and total columns in a table.
  */
 export default function configureAsTeiTableElements(sxModule, options) {
 	options = options || {};
