c6ebd9c3e0a0ee9a4be9dfd9c4da6b53b25ba20a
wvbe <wybe@x-54.com>
2021-12-15 10:47:29 +0000

    DEV-13332 Organize the types for table CVK better, transfer documented remarks

--- src/configureAsTeiTableElements.ts
+++ src/configureAsTeiTableElements.ts
@@ -1,11 +1,9 @@
 import type { SxModule } from 'fontoxml-modular-schema-experience/src/sxManager';
 import configureAsTableElements from 'fontoxml-table-flow/src/configureAsTableElements';
-import type {
-	TableElementsSharedOptions,
-	TableElementsTeiOptions,
-} from 'fontoxml-table-flow/src/types.configureAsTableElements';
+import type { TableElementsSharedOptions } from 'fontoxml-table-flow/src/types';
 
 import TeiTableDefinition from './table-definition/TeiTableDefinition';
+import type { TableElementsTeiOptions } from './types';
 
 /**
  * @remarks
@@ -71,74 +69,6 @@ import TeiTableDefinition from './table-definition/TeiTableDefinition';
  * | our guide} to learn more.
  *
  * @fontosdk importable
- *
- * @param sxModule                            -
- * @param options                             -
- * @param options.priority                    - Selector priority for all elements configured by this
- *                                              function.
- * @param options.allowExpansionInContentView - Defines the availability of expansion of a table.
- * @param options.showInsertionWidget         - To add insertion buttons which insert a column or a row
- *                                              to a specific place, default false.
- * @param options.showHighlightingWidget      - This is deprecated. Instead use showSelectionWidget.
- * @param options.showSelectionWidget         - To add selection bars which select columns and rows,
- *                                              and provide operations popover, default false.
- * @param options.rowBefore                   - Used to add a single icon widget before each row using
- *                                              {@link createIconWidget}. Row widgets are linked to the
- *                                              row elements of the table. Any widget can be added but
- *                                              only icon widget is supported.
- * @param options.columnBefore                - Used to add one or multiple widgets before each column.
- *                                              The context node of each column widget is the cell
- *                                              element in the first row. If there is even one merged
- *                                              cell (to left or right) in the first row, the widgets
- *                                              in columnBefore widget area are not rendered. {@link
- *                                              fonto-documentation/docs/generated-content/editor-api/index.xml#id-cd5577eb-9790-92d6-e3ac-8d1554fe6b12
- *                                              | All widgets} are supported.
- * @param options.columnWidgetMenuOperations  - This is deprecated. Use columnsWidgetMenuOperations instead.
- * @param options.columnsWidgetMenuOperations - To configure table widget menu for columns. It accepts an array of
- *                                              {@link ContextualOperation}s, but only supports "name" and "contents" properties.
- *                                              It is allowed to have only one layer of menu.
- * @param options.rowWidgetMenuOperations     - This is deprecated. Use rowsWidgetMenuOperations instead.
- * @param options.rowsWidgetMenuOperations    - To configure table widget menu for rows. It accepts an
- *                                              array of {@link ContextualOperation}s, but only supports
- *                                              "name" and "contents" properties. It is allowed to have
- *                                              only one layer of menu.
- * @param options.table                       - Options for the table element.
- * @param options.table.namespaceURI          - The namespace URI for this table.
- * @param options.row                         - Configuration options for the row element.
- * @param options.row.headerAttribute         - Configuration options for an attribute to be set on a
- *                                              row element when it is a header row.
- * @param options.row.headerAttribute.name    - The attribute name.
- * @param options.row.headerAttribute.value   - The value to set.
- * @param options.row.regularAttribute        - Configuration options for an attribute to be set on a
- *                                              row element when it is not a header row.
- * @param options.row.regularAttribute.name   - The attribute name.
- * @param options.row.regularAttribute.value  - The value to set.
- * @param options.cell                        - Configuration options for the cell element.
- * @param options.cell.defaultTextContainer   - The default text container for the cell element.
- * @param options.cell.headerAttribute        - Configuration options for an attribute to be set on a
- *                                              cell element when it is a header cell.
- * @param options.cell.headerAttribute.name   - The attribute name.
- * @param options.cell.headerAttribute.value  - The value to set.
- * @param options.cell.regularAttribute       - Configuration options for an attribute to be set on a
- *                                              cell element when it is not a header cell.
- * @param options.cell.regularAttribute.name  - The attribute name.
- * @param options.cell.regularAttribute.value - The value to set.
- * @param options.useDefaultContextMenu       - Whether or not to use a preconfigured context menu for
- *                                              elements within the table.
- * @param options.isCollapsibleQuery          - The {@link XPathQuery} to determine whether or not a
- *                                              table has the ability to be collapsible. Optional,
- *                                              defaults to 'false()'. $rowCount and $columnCount
- *                                              helper variables can optionally be used in this XPath
- *                                              expression which evaluate to the total rows and total
- *                                              columns in a table.
- * @param options.isInitiallyCollapsedQuery   - The {@link XPathQuery} to determine whether or not a
- *                                              table should initially start off as collapsed. Tables
- *                                              must first have the ability to be collapsible with
- *                                              isCollapsibleQuery. Optional, defaults to 'true()'.
- *                                              $rowCount and $columnCount helper variables can
- *                                              optionally be used in this XPath expression which
- *                                              evaluate to the total rows and total columns in a
- *                                              table.
  */
 export default function configureAsTeiTableElements(
 	sxModule: SxModule,
---
+++ src/types.ts
@@ -0,0 +1,142 @@
+import type { DefaultTextContainer } from 'fontoxml-families/src/types';
+
+/**
+ * @remarks
+ * The options accepted by {@link configureAsTeiTableElements}. Please see
+ * {@link TableElementsSharedOptions} for more information on the other
+ * options accepted by this function.
+ *
+ * @fontosdk
+ */
+export type TableElementsTeiOptions = {
+	/**
+	 * @remarks
+	 * Options for the table element.
+	 *
+	 * @fontosdk
+	 */
+	table?: {
+		/**
+		 * @remarks
+		 * The namespace URI for this table.
+		 *
+		 * @fontosdk
+		 */
+		namespaceURI?: string | null;
+	};
+	/**
+	 * @remarks
+	 * Configuration options for the row element.
+	 *
+	 * @fontosdk
+	 */
+	row?: {
+		/**
+		 * @remarks
+		 * Configuration options for an attribute to be set on a
+		 * row element when it is a header row.
+		 *
+		 * @fontosdk
+		 */
+		headerAttribute?: {
+			/**
+			 * @remarks
+			 * The attribute name.
+			 *
+			 * @fontosdk
+			 */
+			name?: string;
+			/**
+			 * @remarks
+			 * The value to set.
+			 *
+			 * @fontosdk
+			 */
+			value?: string;
+		};
+		/**
+		 * @remarks
+		 * Configuration options for an attribute to be set on a
+		 * row element when it is not a header row.
+		 *
+		 * @fontosdk
+		 */
+		regularAttribute?: {
+			/**
+			 * @remarks
+			 * The attribute name.
+			 *
+			 * @fontosdk
+			 */
+			name?: string;
+			/**
+			 * @remarks
+			 * The value to set.
+			 *
+			 * @fontosdk
+			 */
+			value?: string;
+		};
+	};
+	/**
+	 * @remarks
+	 * Configuration options for the cell element.
+	 *
+	 * @fontosdk
+	 */
+	cell?: {
+		/**
+		 * @remarks
+		 * The default text container for the cell element.
+		 *
+		 * @fontosdk
+		 */
+		defaultTextContainer?: DefaultTextContainer;
+		/**
+		 * @remarks
+		 * Configuration options for an attribute to be set on a
+		 * cell element when it is a header cell.
+		 *
+		 * @fontosdk
+		 */
+		headerAttribute?: {
+			/**
+			 * @remarks
+			 * The attribute name.
+			 *
+			 * @fontosdk
+			 */
+			name?: string;
+			/**
+			 * @remarks
+			 * The value to set.
+			 *
+			 * @fontosdk
+			 */
+			value?: string;
+		};
+		/**
+		 * @remarks
+		 * Configuration options for an attribute to be set on a
+		 * cell element when it is not a header cell.
+		 *
+		 * @fontosdk
+		 */
+		regularAttribute?: {
+			/**
+			 * @remarks
+			 * The attribute name.
+			 *
+			 * @fontosdk
+			 */
+			name?: string;
+			/**
+			 * @remarks
+			 * The value to set.
+			 *
+			 * @fontosdk
+			 */
+			value?: string;
+		};
+	};
+};
